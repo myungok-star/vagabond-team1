28d334a39ad28ce4c6c6bf80fd23425e067d869a
myungok-star <myungok.star@gmail.com>
2017-01-03 11:57:19 -0800

    auth done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
 root to: 'sessions#index'
 
+# these routes are for showing users a login form, logging them in, and logging them out.
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+
 get '/signup' => 'users#new'
 post '/users' => 'users#create'
 
